aad1097215546a32d48868c78dc985c3f36deaf5
constanttt <constantlambersens@gmail.com>
2019-09-18 16:04:40 +0200

    Bandage working with regeneration effect

--- src/main/java/net/easier/game/BandageItem.java
+++ src/main/java/net/easier/game/BandageItem.java
@@ -1,12 +1,12 @@
 package net.easier.game;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.*;
 import net.minecraft.world.World;
 
 public class BandageItem extends Item {
@@ -15,10 +15,29 @@ public class BandageItem extends Item {
         super(settings);
     }
 
-    @Override
-    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand)
-    {
-        playerEntity.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
-        return new TypedActionResult<>(ActionResult.SUCCESS, playerEntity.getStackInHand(hand));
+    public int getMaxUseTime(ItemStack itemStack) {
+        return 64;
+    }
+
+    public UseAction getUseAction(ItemStack itemStack) {
+        return UseAction.EAT;
+    }
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        playerEntity.setCurrentHand(hand);
+        return new TypedActionResult(ActionResult.SUCCESS, playerEntity.getStackInHand(hand));
+    }
+
+    public ItemStack finishUsing(ItemStack itemStack, World world, LivingEntity livingEntity) {
+        super.finishUsing(itemStack, world, livingEntity);
+        livingEntity.addPotionEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 20, 2));
+        itemStack.decrement(1);
+        return itemStack;
+    }
+
+    public boolean useOnEntity(ItemStack itemStack, PlayerEntity playerEntity, LivingEntity livingEntity, Hand hand) {
+        livingEntity.addPotionEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 20, 2));
+        itemStack.decrement(1);
+        return true;
     }
 }
